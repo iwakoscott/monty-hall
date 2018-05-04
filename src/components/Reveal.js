dd574e9f0f57911988eb065b90de914b89c0c2b8
Satoshi Scott Iwako <iwakoscott@gmail.com>
2018-05-04 16:06:24 -0700

    add basic file for Reveal.js

---
+++ src/components/Reveal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Reveal extends Component {
+  render(){
+    return (
+      <h1 className="display-2">The big reveal!</h1>
+    );
+  } // render
+} // Reveal
+
+export default Reveal;
